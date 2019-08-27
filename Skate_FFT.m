bab38a5b575b615d8df71b666979daa489d37621
d-robles <drobles@ualberta.ca>
2019-08-27 13:07:00 -0600

    updated scripts to plot spectra by preference and facing orientation

--- Skate_FFT.m
+++ Skate_FFT.m
@@ -4,13 +4,17 @@
 ccc
 %
 exp = 'Skateboard';
-subs = {'104' '106' '107' '108' '109' '110' '111'...
-    '112' '113' '114' '115' '116' '117' };
+subs = {'100' '101' '102' '103' '104' '106'	'107' '108'	'109' '111'	'112' '113'	'116'...
+        '117' '119'	'122' '123'	'124' '125'	'126' '127'};
+is_goofy = [0,0,0,0,0,0,1,0,1,0,0,1,0,0,0,1,0,0,0,0,0];
+
 %subs = {'100'}; %to test on just one sub 
 
 nsubs = length(subs); 
 conds =  {'P_CW';'P_CCW'; 'NP_CW'; 'NP_CCW'};
 conds_lab = {'Preferred Clockwise'; 'Preferred Counterclockwise'; 'Non-preferred Clockwise'; 'Non-preferred Counterclockwise'};
+pref_lab = {'Preferred'; 'Non-Preferred'};
+facing_lab = {'Facing Inside'; 'Facing Outside'};
 nconds = length(conds);
 Pathname = 'M:\Data\Skateboard\Winter2019/';
 [ALLEEG EEG CURRENTSET ALLCOM] = eeglab;
@@ -43,7 +47,7 @@ freqs = [1:1:30]; %wavelet frequencies
 power_out = [];
 i_count = 0;
 n_electrode = EEG.nbchan;
-for i_sub = 1:nsubs 
+for i_sub = 1:nsubs
     fprintf(['Subject - ' num2str(i_sub) '. \n']);
     for i_cond = 1:nconds
         fprintf(['Condition - ' conds_lab{i_cond} '. \n']);
@@ -59,10 +63,19 @@ for i_sub = 1:nsubs
             
         end
         power_out(:,:,:,i_sub,i_cond) = nanmean(power,4); %save the power data
- 
+        
         
     end
- 
+    
+    if is_goofy(i_sub) == 1
+        power_out_face_in(:,:,:,i_sub) = squeeze(nanmean(power_out(:,:,:,i_sub,2:3),5))
+        power_out_face_out(:,:,:,i_sub) = squeeze(nanmean(power_out(:,:,:,i_sub,[1,4]),5))
+    elseif is_goofy(i_sub) == 0
+        power_out_face_in(:,:,:,i_sub) = squeeze(nanmean(power_out(:,:,:,i_sub,[1,4]),5))
+        power_out_face_out(:,:,:,i_sub) = squeeze(nanmean(power_out(:,:,:,i_sub,2:3),5))
+    end
+    power_out_p(:,:,:,i_sub) = squeeze(nanmean(power_out(:,:,:,i_sub,1:2),5)); %averages preferred blocks 1:2
+    power_out_np(:,:,:,i_sub) = squeeze(nanmean(power_out(:,:,:,i_sub,3:4),5)); %save the power data
 end
 
 %% plot spectrograms for each subject (rows) and condition (columns)
@@ -96,17 +109,39 @@ ylabel('Frequency (Hz)');
 
 power_spectra = squeeze(mean(power_out,2)); %collapse over time can pick time range here in 2nd dim.
 %frequency x electrode x subject x condition
+power_spectra_p = squeeze(mean(power_out_p,2));
+power_spectra_np = squeeze(mean(power_out_np,2));
+power_spectra_face_in = squeeze(mean(power_out_face_in,2)); 
+power_spectra_face_out = squeeze(mean(power_out_face_out,2)); 
+
 
 power_spectra_diff = squeeze( power_spectra(:,left_electrode,:,:)-...
                             power_spectra(:,right_electrode,:,:));
-
+power_spectra_diff_p = squeeze( power_spectra_p(:,left_electrode,:)-...
+                            power_spectra_p(:,right_electrode,:));
+power_spectra_diff_np = squeeze( power_spectra_np(:,left_electrode,:)-...
+                            power_spectra_np(:,right_electrode,:));
+power_spectra_diff_face_in = squeeze( power_spectra_face_in(:,left_electrode,:)-...
+                            power_spectra_face_in(:,right_electrode,:)); 
+power_spectra_diff_face_out = squeeze( power_spectra_face_out(:,left_electrode,:)-...
+                            power_spectra_face_out(:,right_electrode,:)); 
+                        
+                        
 power_spectra_diff_mean = squeeze(mean(power_spectra_diff,2));
 power_spectra_diff_se = squeeze(std(power_spectra_diff,[],2)/sqrt(nsubs));
+power_spectra_diff_mean_p = squeeze(mean(power_spectra_diff_p,2));
+power_spectra_diff_se_p = squeeze(std(power_spectra_diff_p,[],2)/sqrt(nsubs));
+power_spectra_diff_mean_np = squeeze(mean(power_spectra_diff_np,2));
+power_spectra_diff_se_np = squeeze(std(power_spectra_diff_np,[],2)/sqrt(nsubs));
+power_spectra_diff_mean_facing_in = squeeze(mean(power_spectra_diff_face_in,2));
+power_spectra_diff_se_facing_in = squeeze(std(power_spectra_diff_face_in,[],2)/sqrt(nsubs));
+power_spectra_diff_mean_facing_out = squeeze(mean(power_spectra_diff_face_out,2));
+power_spectra_diff_se_facing_out = squeeze(std(power_spectra_diff_face_out,[],2)/sqrt(nsubs));
 
 %% Plot spectra accross conditions
 figure;
-boundedline(freqs,power_spectra_diff_mean(:,1),power_spectra_diff_se(:,1),'r',...
-            freqs,power_spectra_diff_mean(:,2),power_spectra_diff_se(:,2),'b',...
+boundedline(freqs,power_spectra_diff_mean(:,1),power_spectra_diff_se(:,1),'k',...
+            freqs,power_spectra_diff_mean(:,2),power_spectra_diff_se(:,2),'m',...
             freqs,power_spectra_diff_mean(:,3),power_spectra_diff_se(:,3),'g',...
             freqs,power_spectra_diff_mean(:,4),power_spectra_diff_se(:,4)),'r';
 axis tight
@@ -117,6 +152,28 @@ ylabel('Power (uV^2)');
 title('Power (Left - Right)');
 legend(conds_lab,'Location','NorthEast');
 
+
+%plot spectra by preference 
+figure;
+boundedline (freqs, power_spectra_diff_mean_p, power_spectra_diff_se_p,'r',...
+             freqs, power_spectra_diff_mean_np, power_spectra_diff_se_np,'b');
+axis tight
+xlim([0 30])
+xlabel('Frequency (Hz)');
+ylabel('Power (uV^2)');
+title('Power (Left - Right)');
+legend(pref_lab,'Location','NorthEast'); 
+
+%plot spectra by facing orientation 
+figure;
+boundedline (freqs, power_spectra_diff_mean_facing_in, power_spectra_diff_se_facing_in,'k',...
+             freqs, power_spectra_diff_mean_facing_out, power_spectra_diff_se_facing_out,'g');
+axis tight
+xlim([0 30])
+xlabel('Frequency (Hz)');
+ylabel('Power (uV^2)');
+title('Power (Left - Right)');
+legend(facing_lab,'Location','NorthEast');
 
 
 
